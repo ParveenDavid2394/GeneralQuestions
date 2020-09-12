36006fe2975bf68e7b3ec88b6b2ab665ed08ff6e
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-12 14:54:09 +0800

    added logic and toggle feature

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach( (question) => {
+    const btn = question.querySelector('.question-btn');
+    
+    btn.addEventListener('click', () =>{
+        // logic where when next question is open, previous question closes automatically
+        questions.forEach( (_question) => {
+            if(_question !== question){
+                _question.classList.remove('show-text');
+            }
+        });
+
+        question.classList.toggle('show-text');
+    });
+});
 
 
 
 // traversing the dom
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach( (btn) => {
-    btn.addEventListener('click', (e) => {
-        const questionParent = e.currentTarget.parentElement.parentElement;
-        questionParent.classList.toggle('show-text');
-    })
-})
+// btns.forEach( (btn) => {
+//     btn.addEventListener('click', (e) => {
+//         const questionParent = e.currentTarget.parentElement.parentElement;
+//         questionParent.classList.toggle('show-text');
+//     })
+// })
