e9916b37f892dd89eb572b0d75aca2e1eec64abe
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-12 14:38:22 +0800

    added button feature to show/hide text

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 //using selectors inside the element
+
+
+
 // traversing the dom
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach( (btn) => {
+    btn.addEventListener('click', (e) => {
+        const questionParent = e.currentTarget.parentElement.parentElement;
+        questionParent.classList.toggle('show-text');
+    })
+})
